78827fd40e415bcf59439a2e17443dff36d990b1
lukkasromero <lukkasromero01@gmail.com>
2024-03-24 22:47:56 -0300

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    configureWebpack: {
+      resolve: {
+        extensions: ['.ts', '.tsx', '.vue', '.js', '.json'],
+      },
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            options: { appendTsSuffixTo: [/\.vue$/] },
+            exclude: /node_modules/,
+          },
+        ],
+      },
+    },
+  };
+  
